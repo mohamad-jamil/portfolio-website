53be3da83d22eb956dc4b5a0e2e1a369120394a5
mohamad-jamil <mohamadjamil03@hotmail.com>
2025-01-22 19:13:53 +0000

    Decrease horizontal margin for bodies of text

--- src/Content.jsx
+++ src/Content.jsx
@@ -10,13 +10,13 @@ export default function Content() {
         alt="profile picture"
         className="w-64 h-64 rounded-full shadow-lg mt-8"
       />
-      <div className="text-white font-mono text-center mt-12 mx-14 lg:max-w-4xl">
+      <div className="text-white font-mono text-center mt-12 mx-6 lg:max-w-4xl">
         Hey! I'm Jamil, a recent graduate from UCL with a passion for software
         development and data science. With a strong foundation in Computer
         Science, I'm well equipped with a solid technical skillset and a
         curiosity to explore and innovate!
       </div>
-      <div className="text-white font-mono text-center mt-4 mx-14 lg:max-w-4xl">
+      <div className="text-white font-mono text-center mt-4 mx-6 lg:max-w-4xl">
         Beyond my career and academics, I enjoy...
       </div>
       <div className="flex items-center justify-center gap-8 mt-8">
