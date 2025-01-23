c528951057da12814e7591c0b1610c32e33cd702
mohamad-jamil <mohamadjamil03@hotmail.com>
2025-01-23 00:42:19 +0000

    Add footer component

---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#0d1213] text-white text-center py-3 font-mono text-sm">
+      &copy; 2025 Mohamad Jamil. All Rights Reserved.
+    </footer>
+  );
+}
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,11 +2,12 @@ import Header from "./Header";
 import Content from "./Content";
 import Projects from "./Projects";
 import Skills from "./Skills";
+import Footer from "./Footer";
 
 export default function Home() {
   return (
     <>
-      <Header /> <Content /> <Projects /> <Skills />
+      <Header /> <Content /> <Projects /> <Skills /> <Footer />
     </>
   );
 }
