08923b3acc1056cda22dff66e20485bf61f896c6
mohamad-jamil <mohamadjamil03@hotmail.com>
2025-01-24 15:22:51 +0000

    Change colour when hovering over skill cards

--- src/SkillCard.jsx
+++ src/SkillCard.jsx
@@ -3,7 +3,7 @@ export default function SkillCard({ skill, link, logo }) {
     <a
       href={link}
       target="_blank"
-      className="bg-[#0d1213] h-36 w-28 p-4 flex flex-col items-center justify-evenly rounded-xl cursor-pointer hover:bg-gray-800"
+      className="bg-[#0d1213] h-36 w-28 p-4 flex flex-col items-center justify-evenly rounded-xl cursor-pointer hover:bg-[#1e2a2f]"
     >
       <img src={logo} alt={`${skill} logo`} className="w-16 h-16" />
       <h4 className="text-white text-center font-mono">{skill}</h4>
